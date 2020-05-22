5500552892830a25e5a1a347f64639e0e908a017
John Cheng <john.cheng@futurice.com>
2020-05-22 20:23:18 +0300

    Improve routing logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:habit_tracker/models/routes.dart';
 import 'package:habit_tracker/models/user.dart';
+import 'package:habit_tracker/screens/home.dart';
 import 'package:habit_tracker/screens/landing.dart';
+import 'package:habit_tracker/screens/login.dart';
 import 'package:habit_tracker/screens/register.dart';
 import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final Routes _routes = Routes();
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -20,7 +24,9 @@ class MyApp extends StatelessWidget {
           title: 'Habit Tracker',
         ),
         routes: {
-          '/register': (context) => RegisterPage(),
+          _routes.getRoute(RouteNames.LOGIN): (context) => LoginPage(),
+          _routes.getRoute(RouteNames.REGISTER): (context) => RegisterPage(),
+          _routes.getRoute(RouteNames.HOME): (context) => HomePage()
         },
       ),
       providers: [
